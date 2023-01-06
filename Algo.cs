5213de6bd9acc853c28b1c1862e5bdf341a45c72
Francis Banini <fkbani@gmail.com>
2023-01-06 16:29:37 +0000

    updated algo file

--- Algo.cs
+++ Algo.cs
@@ -1,8 +1,8 @@
 using System;
-public class Algo {
-
-    public int[] TwoSum(int[] data, int target){
+public class Algo 
+{
+    public int[] TwoSum(int[] data, int target)
+    {
         return new int[]{};
     }
-
 }
